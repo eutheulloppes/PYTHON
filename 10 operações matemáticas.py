6347efdb24c1e20aaa4304b8b86d86008cccb034
Mateus Lopes <129668308+eutheulloppes@users.noreply.github.com>
2023-04-03 09:42:45 -0300

    adicionei via upload

---
+++ 10 operações matemáticas.py
@@ -0,0 +1,63 @@
+calculo1 = 2 + 2
+calculo2 = 2+2*3
+calculo3 = (2+2)*3
+
+print(calculo1)
+print(calculo2)
+print(calculo3)
+
+'''
+OPERADORES DE COMPARAÇÃO
+= Equal
+!= Not Equal
+> Greater than
+< Less than
+>= Greater than or eqaul to
+<= Less than or equal to
+'''
+operadores1 = 10 == 11
+operadores2 = 10 != 11
+operadores3 = 10 > 11
+operadores4 = 10 < 11
+operadores5 = 10 >= 11
+operadores6 = 10 <= 11
+
+print(operadores1)
+print(operadores2)
+print(operadores3)
+print(operadores4)
+print(operadores5)
+print(operadores6)
+
+'''
+OPERADORES DE ATRIBUIÇÃO : REALIZAR CÁLCULOS MATEMÁTICOS COM CÓDIGOS MAIS SIMPLES
+
+
+'''
+x=10
+x +=5
+print(x)
+
+x1=30
+x1-=5
+print(x1)
+
+x2=40
+x2*=2
+print(x2)
+
+x3=40
+x3/=4
+print(x3)
+
+x4=10
+x4 %= 3 #posso colocar 3 vezes o número 3 dentro do 10 e sobre 1
+print(x4)
+
+x5=10
+x5 %= 6 
+print(x5)
+
+x6=10
+x6 %= 2,3
+print(x6)
